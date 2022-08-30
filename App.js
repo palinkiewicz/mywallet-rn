e8d0eab7e61f6275db66c73d793f4d989c0aa0d8
dakil <pogromca.ap@gmail.com>
2022-08-30 19:39:44 +0200

    added Sign-in with email and password feature

--- App.js
+++ App.js
@@ -1,7 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+import EmailLogin from './components/auth/EmailLogin';
+import EmailSignup from './components/auth/EmailSignup';
 
 export default function App() {
+    // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
 
+    // Handle user state changes
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+    }
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
+
+    function signOut() {
+        auth().signOut().then(() => console.log('User signed out!'));
+    }
+
+    if (initializing) return null;
+
+    // User signed-out view
+    if (!user) {
+        return (
+            <View>
+                <EmailSignup />
+                <EmailLogin />
+            </View>
+        );
+    }
+
+    // User logged-in view
+    return (
+        <View>
+            <Text>Welcome {user.email}</Text>
+            <Button title='Sign-out' onPress={signOut} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
---
+++ components/auth/EmailLogin.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native'
+import auth from '@react-native-firebase/auth';
+
+export default function EmailLogin() {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+
+    function enteredEmailHandler(enteredText) {
+        setEnteredEmail(enteredText);
+    }
+
+    function enteredPasswordHandler(enteredText) {
+        setEnteredPassword(enteredText);
+    }
+
+    function logInUser() {
+        if (enteredEmail == '') {
+            return console.error('Please enter an email.')
+        } else if (enteredPassword == '') {
+            return console.error('Please enter a password.')
+        }
+
+        auth().signInWithEmailAndPassword(enteredEmail, enteredPassword)
+            .then(() => {
+                console.log('User signed in!');
+            })
+            .catch(error => {
+                if (error.code == 'auth/user-not-found') {
+                    console.log('User not found.')
+                } else if (error.code == 'auth/wrong-password') {
+                    console.log('Wrong password.')
+                } else if (error.code === 'auth/invalid-email') {
+                    console.log('That email address is invalid!');
+                }
+
+                console.error(error.code);
+            });
+    }
+
+    return (
+        <View>
+            <TextInput placeholder='email' onChangeText={enteredEmailHandler} value={enteredEmail} />
+            <TextInput placeholder='password' onChangeText={enteredPasswordHandler} value={enteredPassword} />
+            <Button title='Log-in' onPress={logInUser} />
+        </View>
+    )
+}
