338bd9820fc7a4f2034f8f51e9ea4125bb96783c
dakil <pogromca.ap@gmail.com>
2022-08-30 18:32:41 +0200

    prebuilt with expo; installed react-native-firebase/app and /auth

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+
 }
 
 const styles = StyleSheet.create({
