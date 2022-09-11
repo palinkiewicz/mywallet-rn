53e48912124a21613808b6c6704a6b534b450808
dakil <pogromca.ap@gmail.com>
2022-09-11 20:02:36 +0200

    Add a function that allows to get all the user's accounts

---
+++ components/logic/accounts/GetAccounts.js
@@ -0,0 +1,33 @@
+import firestore from '@react-native-firebase/firestore';
+
+/**
+ * A function that returns a Firestore subscription,
+ * in which it searches for every account associated with the provided userId
+ * and sets a state value to the collected data using the provided setAccounts setter.
+ *
+ * Should be used inside the useEffect hook.
+ */
+export default function getUserAccounts(user, setAccounts) {
+    const subscriber = firestore()
+        .collection('Accounts')
+        .where('user_id', '==', user.uid)
+        .onSnapshot({
+            error: (e) => {
+                /**
+                 * [firestore/permission-denied] appears somehow, but it doesn't break anything. (See #11)
+                 */
+                console.log(e.message);
+            },
+            next: (querySnapshot) => {
+                const accounts = [];
+
+                querySnapshot.forEach((doc) => {
+                    accounts.push(doc.data());
+                });
+
+                setAccounts(accounts);
+            },
+        });
+
+    return subscriber;
+}
