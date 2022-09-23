cb1a8373a680d8be7d720c9db73606b15ec0219b
dakil <pogromca.ap@gmail.com>
2022-09-23 21:43:35 +0200

    Add route's title and drawer/back button on the Navigation Bar

--- components/ui/PaperNavigationBar.js
+++ components/ui/PaperNavigationBar.js
@@ -1,10 +1,21 @@
 import { Appbar } from 'react-native-paper';
+import { useRoute } from '@react-navigation/native';
+import { MAIN_SCREENS } from '../../screens/_ScreensData';
 
 export default function CustomNavigationBar({ navigation, back }) {
+    const routeName = useRoute().name;
+    const screen = MAIN_SCREENS.find((screen) => screen.name === routeName);
+    const onDrawer = !screen ? false : !screen.onDrawer ? false : true;
+
     return (
         <Appbar.Header>
-            {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-            <Appbar.Content title="My awesome app" />
+            {onDrawer && (
+                <Appbar.Action onPress={navigation.openDrawer} icon="menu" />
+            )}
+            {back && !onDrawer ? (
+                <Appbar.BackAction onPress={navigation.goBack} />
+            ) : null}
+            <Appbar.Content title={' ' + routeName} />
         </Appbar.Header>
     );
 }
