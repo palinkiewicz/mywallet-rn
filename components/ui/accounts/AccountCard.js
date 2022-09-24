bb868366bdefc7d935c8f90755b0fc6f0f752863
dakil <pogromca.ap@gmail.com>
2022-09-24 19:37:51 +0200

    Add AccountCard component

---
+++ components/ui/accounts/AccountCard.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Card, Avatar, IconButton, Button, Menu } from 'react-native-paper';
+
+export default function AccountCard({ icon, title, amount }) {
+    const [menuShown, setMenuShown] = useState(false);
+
+    return (
+        <Card style={styles.card}>
+            <Card.Title
+                title={title}
+                subtitle={'$' + amount}
+                left={(props) => <Avatar.Icon {...props} icon={icon} />}
+                right={(props) => (
+                    <View style={{ flexDirection: 'row' }}>
+                        <Menu
+                            visible={menuShown}
+                            onDismiss={() => setMenuShown(false)}
+                            anchor={<IconButton {...props} icon="dots-vertical" onPress={() => setMenuShown(true)} />}>
+                            <Menu.Item onPress={() => {}} leadingIcon="pencil-outline" title="Edit" />
+                            <Menu.Item onPress={() => {}} leadingIcon="delete-outline" title="Delete" />
+                        </Menu>
+                    </View>
+                )}
+            />
+            <Card.Actions>
+                <Button onPress={() => {}}>See history</Button>
+                <Button onPress={() => {}}>Add record</Button>
+            </Card.Actions>
+        </Card>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        marginHorizontal: 8,
+        marginTop: 8,
+    },
+});
