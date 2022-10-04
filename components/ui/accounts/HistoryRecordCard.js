f372df44c489609b9d7c626c0fe1fba48bacb9eb
dakil <pogromca.ap@gmail.com>
2022-10-04 19:29:40 +0200

    Add HistoryRecordCard component

---
+++ components/ui/accounts/HistoryRecordCard.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Card, IconButton, Menu } from 'react-native-paper';
+
+export default function HistoryRecordCard({
+    index,
+    value,
+    name,
+    date,
+    setRemoveData,
+}) {
+    const [menuShown, setMenuShown] = useState(false);
+
+    const dateObj = new Date(date);
+    const dateString = `${dateObj.getDate()}/${
+        dateObj.getMonth() + 1
+    }/${dateObj.getFullYear()}`;
+
+    return (
+        <Card style={styles.card} mode="elevated">
+            <Card.Title
+                title={'$' + value}
+                subtitle={name + (name === '' ? '' : ' - ') + dateString}
+                right={(props) => (
+                    <View style={{ flexDirection: 'row' }}>
+                        <Menu
+                            visible={menuShown}
+                            onDismiss={() => setMenuShown(false)}
+                            anchor={
+                                <IconButton
+                                    {...props}
+                                    icon="dots-vertical"
+                                    onPress={() => setMenuShown(true)}
+                                />
+                            }
+                        >
+                            <Menu.Item
+                                onPress={() => {}}
+                                leadingIcon="pencil-outline"
+                                title="Edit"
+                            />
+                            <Menu.Item
+                                onPress={() => {
+                                    setMenuShown(false);
+                                    setRemoveData((current) => ({
+                                        ...current,
+                                        active: true,
+                                        index: index,
+                                    }));
+                                }}
+                                leadingIcon="delete-outline"
+                                title="Delete"
+                            />
+                        </Menu>
+                    </View>
+                )}
+            />
+        </Card>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        marginHorizontal: 8,
+        marginTop: 8,
+    },
+});
