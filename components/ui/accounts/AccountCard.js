0bb02951ec7dfed644d191b2c9c5e6f0ae213f9e
dakil <pogromca.ap@gmail.com>
2023-07-16 21:55:27 +0200

    Redesign account card and add ripple to it

--- components/ui/accounts/AccountCard.js
+++ components/ui/accounts/AccountCard.js
@@ -1,70 +1,87 @@
 import { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Card, Avatar, IconButton, Button, Menu } from 'react-native-paper';
+import { Avatar, IconButton, Button, TouchableRipple, useTheme, Text } from 'react-native-paper';
 
 export default function AccountCard({ id, icon, title, amount, setRemoveData, navigation }) {
     const [menuShown, setMenuShown] = useState(false);
+    const { colors } = useTheme();
+
+    const onShowHistory = () => {
+        navigation.navigate('Account history', {
+            accountId: id,
+        });
+    };
+
+    const onEdit = () => {
+        setMenuShown(false);
+        navigation.navigate('Edit account', {
+            _docId: id,
+            _name: title,
+            _icon: icon,
+        });
+    };
+
+    const onDelete = () => {
+        setMenuShown(false);
+        setRemoveData({
+            active: true,
+            accountId: id,
+        });
+    };
+
+    const onAddRecord = () => {
+        navigation.navigate('Add account history', {
+            accountId: id,
+        });
+    };
 
     return (
-        <Card style={styles.card} mode="outlined">
-            <Card.Title
-                title={title}
-                subtitle={amount + ' zł'}
-                left={(props) => <Avatar.Icon {...props} icon={icon} />}
-                right={(props) => (
-                    <Menu
-                        visible={menuShown}
-                        onDismiss={() => setMenuShown(false)}
-                        anchor={<IconButton {...props} icon="dots-vertical" onPress={() => setMenuShown(true)} />}
-                    >
-                        <Menu.Item
-                            onPress={() => {
-                                setMenuShown(false);
-                                navigation.navigate('Edit account', {
-                                    _docId: id,
-                                    _name: title,
-                                    _icon: icon,
-                                });
-                            }}
-                            leadingIcon="pencil-outline"
-                            title="Edit"
-                        />
-                        <Menu.Item
-                            onPress={() => {
-                                setMenuShown(false);
-                                setRemoveData({
-                                    active: true,
-                                    accountId: id,
-                                });
-                            }}
-                            leadingIcon="delete-outline"
-                            title="Delete"
-                        />
-                    </Menu>
-                )}
-            />
-            <Card.Actions>
-                <Button
-                    onPress={() => {
-                        navigation.navigate('Account history', {
-                            accountId: id,
-                        });
-                    }}
-                    mode="contained-tonal"
-                >
-                    See history
-                </Button>
-                <Button
-                    onPress={() => {
-                        navigation.navigate('Add account history', {
-                            accountId: id,
-                        });
+        <TouchableRipple
+            borderless
+            style={{
+                borderRadius: 12,
+                marginHorizontal: 8,
+                marginTop: 8,
+                borderColor: colors.outline,
+                borderWidth: 1,
+                backgroundColor: colors.surface,
+            }}
+            onPress={onShowHistory}
+        >
+            <View>
+                <View style={{ padding: 16, flexDirection: 'row', alignItems: 'center' }}>
+                    <Avatar.Icon icon={icon} size={40} />
+                    <View style={{ marginLeft: 16 }}>
+                        <Text variant="bodyLarge" numberOfLines={1} style={{ marginBottom: 4 }}>
+                            {title}
+                        </Text>
+                        <Text numberOfLines={1} style={{ marginBottom: 2 }}>
+                            {amount + ' zł'}
+                        </Text>
+                    </View>
+                </View>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingRight: 16,
+                        paddingLeft: 8,
+                        paddingBottom: 12,
                     }}
                 >
-                    Add record
-                </Button>
-            </Card.Actions>
-        </Card>
+                    <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+                        <IconButton icon="history" onPress={onShowHistory} />
+                        <IconButton icon="delete-outline" onPress={onDelete} />
+                        <IconButton icon="pencil-outline" onPress={onEdit} />
+                    </View>
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }}>
+                        <Button mode="contained-tonal" onPress={onAddRecord}>
+                            Add record
+                        </Button>
+                    </View>
+                </View>
+            </View>
+        </TouchableRipple>
     );
 }
 
